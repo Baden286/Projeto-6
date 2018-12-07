453328fb069bf1504b1aed663b414ca94b5eb320
Mateus Vidal Marinho <mateus.marinho@fatec.sp.gov.br>
2018-12-07 16:43:07 -0200

    Deixando alunos pré cadastrados

--- sql/banco.sql
+++ sql/banco.sql
@@ -1,31 +1,29 @@
-DROP TABLE usuario;
 -- Criando Tabela Usuario
 CREATE TABLE usuario(
-	id int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
-	login varchar(20) not null,
-	senha varchar(20) not null
+    id int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
+    login varchar(20) not null,
+    senha varchar(20) not null
+);
+
+-- Criando Tabela Curso
+CREATE TABLE curso(
+    id int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    nome varchar(200) not null,
+    sigla varchar(20) not null
+);
+
+-- Criando Tabela Aluno
+CREATE TABLE aluno(
+    matricula int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    nome varchar(200) not null,
+    curso int not null,
+    periodo varchar(20) not null,
+    constraint fk_aluno_curso foreign key (curso) references curso (id)
 );
 
 -- Inserindo usuarios
 INSERT INTO usuario Values
 (default,'admin', 'admin');
-
-DROP TABLE curso;
-CREATE TABLE curso(
-	id int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-	nome varchar(200) not null,
-	sigla varchar(20) not null
-);
-
-DROP TABLE aluno;
--- Criando Tabela Aluno
-CREATE TABLE aluno(
-	matricula int not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-	nome varchar(200) not null,
-	curso int not null,
-	periodo varchar(20) not null,
-        constraint fk_aluno_curso foreign key (curso) references curso (id)
-);
 
 -- Inserindo cursos
 INSERT INTO curso Values
@@ -33,3 +31,9 @@ INSERT INTO curso Values
 (default, 'Processos Químicos', 'PQ'),
 (default, 'Comércio Exterior', 'COMEX'),
 (default, 'Gestão Empresarial', 'GE');
+
+-- Inserindo alunos
+INSERT INTO aluno Values
+(default, 'Anderson do Carmo Nunes', 1, 'Verspertino'),
+(default, 'Baden Lopes Damasio', 1, 'Noturno'),
+(default, 'Mateus Vidal Marinho', 1, 'Verspertino');
